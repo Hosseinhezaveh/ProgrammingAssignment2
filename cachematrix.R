281cc75071737290eb2e41bcaab6bcb73afb1b47
Hosseinhezaveh <hossein.hezaveh@gmail.com>
2017-07-29 03:07:52 -0400

    new Commit

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,12 @@
 ## functions do
 
 ## Write a short comment describing this function
+## The following functions are used to create a special object that stores a matrix and caches its inverse. 
+## The first function, makeCacheMatrix creates a special “matrix”, which is really a list containing a function to:
+## set the value of the matrix
+## get the value of the matrix
+## set the value of the inverse
+## get the value of the inverse
 
 makeCacheMatrix <- function(x=matrix){
   inv <- NULL
@@ -19,6 +25,8 @@ makeCacheMatrix <- function(x=matrix){
 
 
 ## Write a short comment describing this function
+## This function computes the inverse of the special “matrix” returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then cacheSolve should retrieve the inverse from the cache.
 
 cacheinv <- function(x, ...) {
   inv <- x$getinv()
